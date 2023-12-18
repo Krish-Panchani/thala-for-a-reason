bdc8ea82e51e45a3bb056a51b2e9ead53fb0480b
Krish-Panchani <krishpanchani1346@gmail.com>
2023-12-18 21:40:57 +0530

    updated copy link

--- thala.php
+++ thala.php
@@ -48,7 +48,7 @@
                 </audio> -->
 
                 <div class=' mx-auto flex flex-col justify-center items-center'>
-                    <input type="text" id="copyMe" name="copyMe" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm m-2 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mx-auto w-1/2" value="https://localhost/special/index.php?thala=<?= $searchParam ?>" placeholder="link" disabled>
+                    <input type="text" id="copyMe" name="copyMe" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm m-2 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mx-auto w-1/2" value="https://www.thunderdevelops.in/thala-for-a-reason/thala.php?thala=<?= $searchParam ?>" placeholder="link" disabled>
                     <button class='flex items-center justify-center p-0.5 m-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-purple-200  mx-auto' onclick="copyMeOnClipboard()">
                         <span class='flex px-4 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0'>
                             <img src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/16/null/external-link-marketing-seo-flatart-icons-flat-flatarticons.png" />
