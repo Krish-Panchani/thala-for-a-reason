379b44d127cb13b8fd154ac0ef5c77d5a28e37b9
Krish Panchani <krishpanchani1346@gmail.com>
2023-12-18 23:02:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </form>
 
 
-    <footer class="bg-white rounded-lg shadow m-4  flex flex-col min-h-screen">
+    <footer class="bg-white rounded-lg shadow m-4">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-500 sm:text-center ">© 2023 <a href="/" class="hover:underline">Thunder Develops</a>. All Rights Reserved.
             </span>
